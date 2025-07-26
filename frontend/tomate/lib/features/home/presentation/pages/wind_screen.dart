f52f2df835cb0392336cb417a9032e2d61053ef7
주승현 <tmdgus4475@gmail.com>
2025-07-27 04:22:08 +0900

    로티빼고 ios 완료

--- frontend/tomate/lib/features/home/presentation/pages/wind_screen.dart
+++ frontend/tomate/lib/features/home/presentation/pages/wind_screen.dart
@@ -31,7 +31,6 @@ class _WindScreenState extends State<WindScreen> {
         children: [
           Expanded(
             child: SingleChildScrollView(
-              padding: EdgeInsets.all(20),
               child: Column(
                 children: [
                   // 호흡 애니메이션 GIF
@@ -41,39 +40,69 @@ class _WindScreenState extends State<WindScreen> {
                   ),
 
                   // 안내 메시지
-                  Container(
-                    padding: EdgeInsets.all(20.w),
-                    margin: EdgeInsets.only(bottom: 40.h),
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(12.r),
-                      border: Border.all(
-                        color: const Color(0xFFE0E0E0),
-                        width: 1,
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 26),
+                    child: Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.only(bottom: 40),
+                      decoration: ShapeDecoration(
+                        color: Colors.white,
+                        shape: RoundedRectangleBorder(
+                          side: BorderSide(
+                            width: 1,
+                            color: const Color(0xFF4D4D4D),
+                          ),
+                          borderRadius: BorderRadius.circular(12),
+                        ),
                       ),
-                    ),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          '천천히 어깨에 힘빼시고 호흡해주세요.',
-                          style: TextStyle(
-                            fontSize: 14.sp,
-                            fontWeight: FontWeight.w600,
-                            color: Colors.black,
-                          ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 18),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text.rich(
+                              TextSpan(
+                                children: [
+                                  TextSpan(
+                                    text: '승현님 이전에 ',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: '5번',
+                                    style: TextStyle(
+                                      color: const Color(0xFFEA423C),
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: '이나 잘 이겨냈어요.',
+                                    style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 12),
+                            Text(
+                              '지금 두근거림은 단지 불안의 일시적인 반응일 뿐이고\n단비님의 심장은 건강해요. 수없이 이런 순간을 지나\n왔으니까 분명 단비님은 이번에도 잘 해낼 거예요!',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 12,
+                                fontWeight: FontWeight.w500,
+                                height: 1.60,
+                              ),
+                            )
+                          ],
                         ),
-                        SizedBox(height: 8.h),
-                        Text(
-                          '지금 투약으로부터 얻은 컨디션 정보를 통해 알아낸 결과입니다. 아래의 대화를 통해 지금의 컨디션에 대해 충분히 이해하고 좋은 컨디션 유지를 위해 계속 기여하세요.',
-                          style: TextStyle(
-                            fontSize: 12.sp,
-                            fontWeight: FontWeight.w400,
-                            color: const Color(0xFF666666),
-                            height: 1.4,
-                          ),
-                        ),
-                      ],
+                      ),
                     ),
                   ),
                 ],
