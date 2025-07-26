0d4b4880a73ceafd922bccfba9f3a09db8ac71c5
TaehoLEEKR <679748@naver.com>
2025-07-26 16:24:14 +0900

    feat: user controller 추가

---
+++ backend/kpsa-hack-12/src/main/java/com/hack/kpsahack12/user/controller/UserChatController.java
@@ -0,0 +1,32 @@
+package com.hack.kpsahack12.user.controller;
+import com.hack.kpsahack12.model.dto.UserChatRequestDto;
+import com.hack.kpsahack12.model.dto.UserChatResponseDto;
+import com.hack.kpsahack12.user.service.UserChatService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/chat")
+@RequiredArgsConstructor
+@Tag(name = "LLM Chat", description = "Gemmini LLM 채팅 기능 구현 ")
+public class UserChatController {
+
+    private final UserChatService userChatService;
+
+
+    @Operation(
+            summary = "잼민이랑 대화 하기",
+            description = "프롬프트 현재 - 존댓말 하지 않고 대답 잘하기 -. llmModel 은 NULL able"
+    )
+
+    @PostMapping("/prompt")
+    public Mono<UserChatResponseDto> chattingOneComment(@RequestBody UserChatRequestDto userChatRequestDto) {
+        return userChatService.getchattingOneComment(userChatRequestDto);
+    }
+}
