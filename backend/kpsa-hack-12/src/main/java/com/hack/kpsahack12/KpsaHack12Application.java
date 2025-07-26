020b9c59a2ad9c98901d2b4af5707d66a3fd4b37
TaehoLEEKR <679748@naver.com>
2025-07-26 14:47:10 +0900

    chore : init base back

---
+++ backend/kpsa-hack-12/src/main/java/com/hack/kpsahack12/KpsaHack12Application.java
@@ -0,0 +1,13 @@
+package com.hack.kpsahack12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KpsaHack12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KpsaHack12Application.class, args);
+	}
+
+}
---
+++ backend/kpsa-hack-12/src/test/java/com/hack/kpsahack12/KpsaHack12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hack.kpsahack12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KpsaHack12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
