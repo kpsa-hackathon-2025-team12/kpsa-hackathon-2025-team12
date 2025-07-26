1db9ef4283ecbc20cdb4b04c96771b796dc3b247
TaehoLEEKR <679748@naver.com>
2025-07-26 15:11:21 +0900

    feat: OAuth2 data config init

---
+++ backend/kpsa-hack-12/src/main/java/com/hack/kpsahack12/config/OAUth2/ApiUrlValues.java
@@ -0,0 +1,11 @@
+package com.hack.kpsahack12.config.OAUth2;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiUrlValues {
+    private String logout;
+    private String userInfo;
+}
